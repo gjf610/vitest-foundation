ec96344a0c333e6c54467de0691d3a3c7384be78
jianfeng <gjf610@yeah.net>
2024-08-13 10:16:48 +0800

    feat: vitest porject

---
+++ test.spec.js
@@ -0,0 +1,11 @@
+import { it, test } from 'vitest'
+
+// test it
+
+// it -> BDD 行为驱动开发
+// it should xxx xxx, Mocha Jasmine
+it("should", () => { })
+
+// test -> TDD 测试去哦那个开发
+// test xxx, Jest
+test("xxx", () => { })
